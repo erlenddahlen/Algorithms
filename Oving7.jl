2b2f19de288965b2bd6374b9d25733084ab7787f
blackearl <erlended@stud.ntnu.no>
2018-10-08 21:18:42 +0200

    Oving7

--- Oving7.jl
+++ Oving7.jl
@@ -1,1 +1,28 @@
-x = 2
+function can_use_greedy(coins)
+    for i= 1:(length(coins)-1)
+        a=coins[i]/coins[i+1]
+        if !(round(a)%a==0)
+            return false
+        end
+    end
+    return true
+end
+
+function min_coins_greedy(coins, value)
+    temp = value
+    index = 1
+    number = 0
+    while temp > 0
+        if temp>=coins[index]
+            number = number + 1
+            temp = temp - coins[index]
+        else
+            index = index +1
+        end
+    end
+    return number
+end
+
+function min_coins_dynamic(coins,value)
+    
+end
